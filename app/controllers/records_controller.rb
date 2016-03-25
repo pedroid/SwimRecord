40ea7c3099ec9be092a2954193e1f6eadda21aa9
yushengc <ntuyusheng@gmail.com>
2016-03-25 20:52:18 +0800

    成功在view helper中使用model，將使用者id用ascii 找到

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -1,6 +1,7 @@
 class RecordsController < ApplicationController
   before_action :authenticate_user!
   def index
+    @users = User.all 
     @records= current_user.records.all
   end
   def new
--- app/helpers/records_helper.rb
+++ app/helpers/records_helper.rb
@@ -1,2 +1,35 @@
 module RecordsHelper
+  def program_id_to_ascii_txt(program_id)
+    case program_id
+    when 1
+      return "freestyle"
+    when 2
+      return "breaststroke"
+    when 3
+      return "backstroke"
+    when 4
+      return "butterfly"
+    else
+      return "others"
+    end
+  end
+
+  def property_id_to_ascii_txt(property_id)
+    case property_id
+    when 1
+      return "self-training"
+    when 2
+      return "contest"
+    else
+      return "others"
+    end
+  end
+
+  def uploader_to_ascii_txt(user_id)
+    @users.each do |user|
+      if user.id == user_id
+        return user.name
+      end
+    end
+  end
 end
