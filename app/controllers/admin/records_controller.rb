1caaf23f8ac815cbf3f257295e9a80dcc7f821e1
yushengc <ntuyusheng@gmail.com>
2016-03-25 09:14:00 +0800

    增加使用者功能

---
+++ app/controllers/admin/records_controller.rb
@@ -0,0 +1,25 @@
+class Admin::RecordsController < ApplicationController
+  before_action :authenticate_user!
+  def index
+    @records= Record.all
+  end
+  def new
+    @record = Record.new
+  end
+
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      redirect_to admin_records_path
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def record_params
+    params.require(:record).permit(:program_id, :distance, :property_id, :minutes, :seconds)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
   # root 'welcome#index'
+ namespace :admin do
+	resources :records
+ end
  root 'pages#index'
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
@@ -13,6 +17,7 @@ Rails.application.routes.draw do
 
   # Example resource route (maps HTTP verbs to controller actions automatically):
   #   resources :products
+resources :records
 
   # Example resource route with options:
   #   resources :products do
