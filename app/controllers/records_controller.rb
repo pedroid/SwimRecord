b4a14fd8477fbd71b7c17ecfc7c4ed8331d0f8ef
yushengc <ntuyusheng@gmail.com>
2016-04-03 21:34:03 +0800

    edit and delete function of records

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -26,6 +26,30 @@ end
 def show
 
 end
+
+def update
+  @record = Record.find(params[:id])
+     if @record.update(record_params)
+       redirect_to records_path, notice: "修改成功"
+     else
+       render :edit
+     end
+   
+end
+
+def edit
+  @record = Record.find(params[:id])
+  @swim_items = SwimItem.all
+  @swim_distances = SwimDistance.all
+  @atheletes = Athelete.all
+  @contests = Contest.all
+end
+
+def destroy
+  @record = Record.find(params[:id])
+  @record.destroy
+  redirect_to records_path, alert:"record deleted"
+end
 def record_params
   params.require(:record).permit(:owner_id, :swim_item_id, :distance_id, :contest_id, :minutes, :seconds, :month, :date, :year)
 end
